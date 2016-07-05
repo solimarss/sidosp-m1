5b0ce63deeedca87123420c91ca8df848cb531b2
solimarss <solimarss@rpawsgweb04.redetre.jus.br>
2016-07-05 09:20:59 -0300

    configurado junit com cdi

--- sidosp-m1/src/test/java/br/com/solimar/sidosp/m1/test/DoacaoTest.java
+++ sidosp-m1/src/test/java/br/com/solimar/sidosp/m1/test/DoacaoTest.java
@@ -1,21 +1,18 @@
 package br.com.solimar.sidosp.m1.test;
 
-import java.util.List;
-
 import javax.inject.Inject;
-import javax.persistence.Entity;
-import javax.persistence.EntityManager;
-import javax.persistence.Query;
 
 import org.junit.Test;
+import org.junit.runner.RunWith;
 
 import br.com.solimar.sidosp.core.domain.Doador;
 import br.com.solimar.sidosp.m1.business.DoadorBC;
 
+@RunWith(WeldJUnit4Runner.class)
 public class DoacaoTest {
 	
 	@Inject
-	private Doador doador;
+	private DoadorBC doadorBC;
 	
 	
 	
@@ -23,26 +20,9 @@ public class DoacaoTest {
 	@Test
 	public void agendar(){
 		
-		doador.getName();
-		//Doador doador = doadorBC.findByMailAndPassword("sol", "123");
-		/*try {
-			Query query = em
-					.createQuery("Select D from Doador D Where D.mail =:pMail and D.password =:pPassword", Doador.class);
-			
-			query.setParameter("pMail", "sol");
-			query.setParameter("pPassword", "123");
-			
-			Doador doador = null;
-			List<Doador> lista = query.getResultList();
-			
-			if(lista.size() > 0){
-				doador = lista.get(0);
-			}
-			
-		} catch (Exception e) {
-			e.printStackTrace();
-
-		}*/
+		
+		Doador doador = doadorBC.findByMailAndPassword("sol", "123");
+		
 		
 	}
 
