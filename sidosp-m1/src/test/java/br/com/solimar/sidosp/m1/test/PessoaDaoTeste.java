b2796b190b5170aa15315bf45a13cf32ae1e5a0c
solimarss <solimarss@rpawsgweb04.redetre.jus.br>
2016-07-05 14:30:18 -0300

    Incluído arquillian

---
+++ sidosp-m1/src/test/java/br/com/solimar/sidosp/m1/test/PessoaDaoTeste.java
@@ -0,0 +1,47 @@
+package br.com.solimar.sidosp.m1.test;
+
+import javax.inject.Inject;
+
+import org.jboss.arquillian.container.test.api.Deployment;
+import org.jboss.arquillian.junit.Arquillian;
+import org.jboss.arquillian.junit.InSequence;
+import org.jboss.shrinkwrap.api.Archive;
+import org.jboss.shrinkwrap.api.ShrinkWrap;
+import org.jboss.shrinkwrap.api.asset.EmptyAsset;
+import org.jboss.shrinkwrap.api.spec.WebArchive;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import br.com.solimar.sidosp.m1.business.DoadorBC;
+
+@RunWith(Arquillian.class)
+public class PessoaDaoTeste {
+ 
+
+	@Deployment
+	public static Archive<?> criarArquivoTeste() {
+		Archive<?> arquivoTeste = ShrinkWrap.create(WebArchive.class, "aplicacaoTeste.war")
+				// Adicionando o pacote inteiro da classe PessoaDao, ou seja incluí todas as outras classes deste pacote
+				.addPackage(DoadorBC.class.getPackage())
+				// Adicionando apenas a classe Pessoa, e não o pacote inteiro como na linha anterior
+				.addClass(DoadorBC.class)
+				// Adicionando o arquivo persistence.xml para conexão JPA
+				.addAsResource("META-INF/persistence.xml")
+				// Adicionando o beans.xml para ativação do CDI
+				.addAsWebInfResource(EmptyAsset.INSTANCE, "beans.xml");
+		return arquivoTeste;
+	}
+
+	@Inject
+	DoadorBC doadorBC;
+
+	@Test
+	@InSequence(1)
+	public void testeSalvarPessoa() {
+		
+		doadorBC.findByMailAndPassword("sol", "123");
+
+	}
+
+
+}
